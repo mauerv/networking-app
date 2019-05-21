b5d7cfb4cc8d0d15532c157ca3d4f839b84c6546
mauerv <maurovelazquez1@gmail.com>
2019-05-21 15:43:16 -0300

    Improve edit and show profile functionality.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,11 +3,29 @@ class ProfilesController < ApplicationController
 	end
 
 	def show
+		@profile = Profile.find(params[:id])
 	end
 
-	def edit
-	end
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+  	@profile = Profile.find(params[:id])
+  	if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile updated"
+      redirect_to @profile
+  	else
+  		render 'edit'
+  	end
+  end
 
 	def new
 	end
+
+	private
+
+		def profile_params
+			params.require(:profile).permit(:name, :title, :country, :description, :image)
+		end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,6 +2,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    edit_profile_path(resource)
+    edit_profile_path(resource.profile)
   end
 end
