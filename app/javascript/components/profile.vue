e7d90ef6e5ec5423b57e4291612c74b2393685c9
mauerv <maurovelazquez1@gmail.com>
2019-06-10 18:58:50 -0300

    Compartimentalize profile into a vue component.

---
+++ app/javascript/components/profile.vue
@@ -0,0 +1,102 @@
+<script>
+export default {
+	data: function() {
+		return {
+			notice: '',
+			request_message: '',
+			connection_request: this.request,
+			current_user: this.user			
+		}
+	},
+	props: ['request', 'profile', 'user'],
+  computed: {
+    isContact: function() {
+      return this.current_user.profile.contacts.filter(e => e.id === this.profile.id).length !== 0
+    },
+    isRequestor: function() {
+      return this.current_user.profile.requestors.filter(e => e.id === this.profile.id).length !== 0
+    },
+    isReceiver: function() {
+      return this.current_user.profile.receivers.filter(e => e.id === this.profile.id).length !== 0
+    }
+  },
+  watch: {
+  	notice: function() {
+  		return setTimeout(() => this.notice = '', 2000)
+  	}
+  },
+  methods: { 
+    sendRequest() {
+      let data = {  
+        connection_request: {
+          "request_message": this.request_message,
+          "profile_id": this.current_user.id ,
+          "contact_id": this.profile.id
+        }
+      }
+
+      $.ajax({
+        url: `/request-manager`,
+        type: 'post',
+        data: data,
+        success: data => {
+          this.connection_request = { id: data.id }
+          this.current_user.profile.receivers.push(this.profile)
+          $('#modal-window').modal('toggle')
+          this.notice = 'Request sent.'
+        },
+        error: err => console.log(err)
+      })
+    },
+    removeContact(profile_id) {
+      $.ajax({
+        url: `/contacts/${profile_id}`,
+        type: 'DELETE',
+        success: data => {
+          this.connection_request = 
+          this.current_user.profile.contacts = this.current_user.profile.contacts.filter(e => e.id !== this.profile.id)
+          this.notice = "Contact removed."
+        },
+        error: (err) => console.log(err)
+      })
+    },
+    withdrawRequest(request_id) {
+      $.ajax({ 
+        url: `/request-manager/withdraw/${request_id}`,
+        type: 'DELETE',
+        success: data => {
+          this.current_user.profile.receivers = this.current_user.profile.receivers.filter(e => e.id !== this.profile.id)
+          this.notice = "Request withdrawn."
+        },
+        error: err => console.log(err)
+      })
+    },
+    declineRequest(request_id) {
+      $.ajax({
+        url: `/request-manager/${request_id}`,
+        type: 'DELETE',
+        success: data => {
+          this.current_user.profile.requestors = this.current_user.profile.requestors.filter(e => e.id !== this.profile.id)              
+          this.notice = "Request declined"
+        },
+        error: err => console.log(err)
+      })
+    },
+    acceptRequest(request_id) {
+      $.ajax({
+        url: `/request-manager/${request_id}`,
+        type: 'PATCH',
+        success: data => {
+          this.current_user.profile.requestors = this.current_user.profile.requestors.filter(e => e.id !== this.profile.id)
+          this.current_user.profile.contacts.push(this.profile)           
+          this.notice = "Request accepted"  
+        },
+        error: err => console.log(err)
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>	
