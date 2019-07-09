3e07837742a1b7dc313af7d9c432d956505b2a1c
Mauro <mauro@osn.global>
2019-07-09 16:12:46 +0200

    Standardize all JS variables into camelCase

--- app/javascript/components/feed.vue
+++ app/javascript/components/feed.vue
@@ -5,30 +5,30 @@ export default {
 	props: ['requests', 'contacts'],
 	data() {
 		return {
-			connection_requests: this.requests
+			connectionRequests: this.requests
 		}
 	},
 	computed: {
 		visibleRequests() {
-			return this.connection_requests.slice(0, 3)
+			return this.connectionRequests.slice(0, 3)
 		},
 		contactsNum() {
 			return this.contacts.length
 		},
 		requestsNum() {
-			return this.connection_requests.length
+			return this.connectionRequests.length
 		}
 	},
 	methods: {
     declineRequest(req_id) {
   		declineReq(req_id, () => {
-      	this.connection_requests = this.connection_requests.filter(e => e.id !== req_id)
+      	this.connectionRequests = this.connectionRequests.filter(e => e.id !== req_id)
       	this.$emit('new-notice', "Request declined")
     	})
     },
     acceptRequest(req_id) {
   		acceptReq(req_id, () => {
-      	this.connection_requests = this.connection_requests.filter(e => e.id !== req_id)
+      	this.connectionRequests = this.connectionRequests.filter(e => e.id !== req_id)
       	this.$emit('new-notice', "Request accepted")
     	})
     }
