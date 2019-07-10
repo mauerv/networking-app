c658f152684f1ad1ac24292e3390d7464e051f8a
Mauro <mauro@osn.global>
2019-07-10 14:37:03 +0200

    Add some comments to models

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,9 +1,11 @@
 class Connection < ApplicationRecord
+	# Creates the reciprocal for a connection.
 	after_create do |c|
-		if !Connection.find_by(contact_id: c.profile_id)	
+		if !Connection.find_by(contact_id: c.profile_id)
 			c.contact.connections.create(contact: c.profile)
 		end
 	end
+	# Destroys the reciprocal of a connection.
 	after_destroy do |c|
 		reciprocal = Connection.find_by(contact_id: c.profile_id)
 		reciprocal.destroy unless reciprocal.nil?
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_one :profile, dependent: :destroy
   after_create :init_profile
 
+  # Automatically creates and links a profile for every new user.
   def init_profile
   	self.create_profile!
   end
